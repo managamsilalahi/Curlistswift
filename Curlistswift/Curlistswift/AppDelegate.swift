c010b9df7bbad0d2d54edd0aed48811c1fee0e4b
Managam Silalahi <managam.silalahi@gmail.com>
2017-12-04 23:07:32 +0700

    Make root view controller is tab bar controller

--- Curlistswift/Curlistswift/AppDelegate.swift
+++ Curlistswift/Curlistswift/AppDelegate.swift
@@ -18,7 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = tabBarController()
+        window?.rootViewController = UITabBarController()
+        window?.makeKeyAndVisible()
         
         return true
     }
